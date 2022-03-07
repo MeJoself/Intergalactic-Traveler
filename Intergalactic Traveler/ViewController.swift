0447ee739eff364568d52d0645bd38a0be98dd78
Jose Faustino <josefaustino.channel@gmail.com>
2022-03-07 14:11:11 -0700

    MVP

--- Intergalactic Traveler/ViewController.swift
+++ Intergalactic Traveler/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       assignbackground()
     }
-
+    
+    func assignbackground()
+    {
+        let background = UIImage(named: "Space Pic")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =
+        UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
 
 }
 
